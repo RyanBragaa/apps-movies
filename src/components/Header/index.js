b27f93f7d89a339e8319e3bfc8832d2c11252476
RyanBraga <125956400+RyanBragaa@users.noreply.github.com>
2023-09-07 18:09:45 -0300

    atualizacoes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 function Header() {
   return (
       <header className={styles.header}>
-        
-        <span>BGFlix</span>
+        <Link to="/">
+          <span>BGFlix</span>
+        </Link>
         <nav>
-            <a href="#">Home</a> 
-            <a href="#">Assistir</a>
+          <Link to="/">Home</Link>
+          <Link to="/watch">Assistir</Link>
         </nav>
       </header>
   );
--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -1,10 +1,19 @@
+import Header from "../../components/Header";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
 import styles from "./Watch.module.css";
 
 function Watch() {
     return (
-        <section className={styles.watch}>
-           <h1>Assistir</h1>
-        </section>
+        <>
+            <Header />
+            <Container>
+                <section className={styles.watch}>
+                    <iframe width="854" height="480" src="https://www.youtube.com/embed/BO4RJISPSH0?si=s-j5IcXsoB_DnyZZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </section>
+            </Container>
+            <Footer />
+        </>
     );
 }
 
