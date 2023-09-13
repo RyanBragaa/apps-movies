017cffab456d0bca15f42dc2c24ac86d7c16bbca
RyanBraga <125956400+RyanBragaa@users.noreply.github.com>
2023-09-13 13:47:47 -0300

    atualizações

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,24 @@
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
-import Category, {categories, filterCategory} from "../../components/Category";
+import Category, { categories, filterCategory } from "../../components/Category";
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import Carousel from "../../components/Carousel";
 
 function Home() {
 
   return (
     <>
-      <Header /> 
-      <Banner image="favoritos"/>
+      <Header />
+      <Banner image="favoritos" />
       <Container>
 
-        {categories.map((category, index) => 
+        {categories.map((category, index) =>
           <Category category={category}>
-            { filterCategory(index).map((video) => <Card id={video.id} key={video.id} /> )}
+            <Carousel>
+              {filterCategory(index).map((video) => <Card id={video.id} key={video.id} />)}
+            </Carousel> 
           </Category>
         )}
 
