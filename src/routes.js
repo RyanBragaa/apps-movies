2d54ffa01a08e0ca4e52ad9840667f9df9c1d744
RyanBraga <125956400+RyanBragaa@users.noreply.github.com>
2023-09-14 17:47:49 -0300

    atualizações

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Favorites from "./pages/Favotires";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
 import Search from "./pages/Search";
@@ -11,6 +12,7 @@ function AppRoutes() {
                 <Route  path="/" element={ <Home /> } ></Route>
                 <Route  path="/watch/:id" element={ <Watch /> } ></Route>
                 <Route  path="/search" element={ <Search /> } ></Route>
+                <Route  path="/favorites" element={ <Favorites /> } ></Route>
                 <Route  path="*" element={ <PageNotFound /> } ></Route>
             </Routes>
         </BrowserRouter>
