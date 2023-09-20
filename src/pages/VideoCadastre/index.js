64069b4f1c7d41a74bf5d7100cf4c0435789ed8b
RyanBraga <125956400+RyanBragaa@users.noreply.github.com>
2023-09-20 17:08:20 -0300

    atualizações

---
+++ src/pages/VideoCadastre/index.js
@@ -0,0 +1,19 @@
+import Form from "../../components/Form";
+import Header from "../../components/Header";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+
+
+function VideoCadastre() {
+    return (
+        <>
+            <Header />
+            <Container>
+                <Form />
+            </Container>
+            <Footer />
+        </>
+    );
+}
+
+export default VideoCadastre;
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Form, Route, Routes } from "react-router-dom";
 import FavoritesProvider from "./contexts/Favorites";
 import Favorites from "./pages/Favotires";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
 import Search from "./pages/Search";
 import Watch from "./pages/Watch";
+import VideoCadastre from "./pages/VideoCadastre";
 
 function AppRoutes() {
     return (
@@ -15,6 +16,7 @@ function AppRoutes() {
                     <Route path="/watch/:id" element={<Watch />} ></Route>
                     <Route path="/search" element={<Search />} ></Route>
                     <Route path="/favorites" element={<Favorites />} ></Route>
+                    <Route path="/cadastre" element={<VideoCadastre />} ></Route>
                     <Route path="*" element={<PageNotFound />} ></Route>
                 </Routes>
             </FavoritesProvider>
